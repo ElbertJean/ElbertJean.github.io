1ea6641fc1df4f3060f5703401696f5a2868013f
Elbert Jean <elbertjeandossantos@gmail.com>
2024-11-05 14:44:44 -0300

    teste aplicacao react

--- src/CounterDisplay.js
+++
@@ -1,9 +0,0 @@
-import React, { useContext } from 'react';
-import CountContext from './CountContext';
-
-const CounterDisplay = () => {
-  const { count } = useContext(CountContext);
-  return <h1>Contagem: {count}</h1>;
-};
-
-export default CounterDisplay;
